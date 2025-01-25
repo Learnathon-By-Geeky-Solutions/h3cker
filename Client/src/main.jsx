4e5b8ae7241a0ca3f0859bd6ac5edbcd3034e210
Utchash007 <shariarhasan872@gmail.com>
2025-01-26 00:20:48 +0600

    adding flowbite and flowbite-react

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Routes/Routes/Routes.jsx'
+import "flowbite";
 
 
 createRoot(document.getElementById('root')).render(
--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,12 +1,25 @@
+const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite/**/*.js", // Include Flowbite components
+    "node_modules/flowbite-react/**/*.js", // Include Flowbite React
+    flowbite.content(),
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-gray': '#E0E1DD',
+        'soft-muted-blue-gray':'#778DA9',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'), // Add Flowbite plugin
+    flowbite.plugin(),
+  ],
 }
 
