a43fe5bc6c1daefcaed7c4aa4502d1dc38185e3c
Utchash007 <shariarhasan872@gmail.com>
2025-02-05 11:10:51 +0600

    Added Admin Dashboard For Routing

--- Client/src/Routes/Routes/Routes.jsx
+++ Client/src/Routes/Routes/Routes.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../../App";
 import Home from "../../components/Pages/Home/home.jsx";
 import Login from "../../components/Shared/Login/login.jsx";
-
+import AdminDashboard from "../../components/Pages/AdminDashboard/AdminDashBoard.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -15,6 +15,10 @@ const router = createBrowserRouter([
       {
         path: "/login",
         element: <Login />,
+      },
+      {
+        path:"/admin",
+        element:<AdminDashboard/>
       }
     ]
 
