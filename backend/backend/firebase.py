2c2de83938c9883753b28300ca1053b1a26b8e35
ismailariyan <ariyan.git.8hxlz@aleeas.com>
2025-04-12 01:37:06 +0600

    Refactor Firebase credential path handling for production environment

--- backend/backend/firebase.py
+++ backend/backend/firebase.py
@@ -4,6 +4,9 @@ import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-cred_path = os.path.join(BASE_DIR, "firebase-credentials.json")
+if os.getenv("ENVIRONMENT") == 'PROD':
+    cred_path = os.path.join("/etc/secrets/firebase-credentials-prod.json")
+else:
+    cred_path = os.path.join(BASE_DIR, "firebase-credentials.json")
 cred = credentials.Certificate(cred_path)
 default_app = firebase_admin.initialize_app(cred)
